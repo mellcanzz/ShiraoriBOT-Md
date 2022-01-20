cef3eadbebfaae9005d354726f3f013975a50060
MELL!! <97344921+mellcanzz@users.noreply.github.com>
2022-01-21 01:55:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6281351047727'] // Put your number here
+global.owner = ['6283830815715'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
@@ -31,7 +31,7 @@ global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = 'ShiraoriBOT Multi device'
 global.author = 'Ilman'
 
-global.wm = '© ShiraoriBOT-Md Made by Ilman'
+global.wm = '© XrzzZ'
 global.media = 'https://telegra.ph/file/5d45a3d1e12726d98c5e4.jpg'
 
 global.wait = '_*tunggu sedang di proses...*_'
